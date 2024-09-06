74499398fb69669ff90aeb14245d0b5f37556ae3
GuLeiteAbreu007 <gustavo.abreu@germinare.org.br>
2024-09-06 16:54:16 -0300

    Cosertando comportamento da tela e outras questões

--- js/scriptScript.js
+++ js/scriptScript.js
@@ -1,5 +1,7 @@
 import {atualizarDisciplinas} from './scriptConexao.js'
 import {atualizarSeries} from './scriptConexao.js'
+import {atualizarHorarios} from './scriptConexao.js'
+
 let alunosPorHorario = {};
 let status = ["Livre", "Indisponível", "Ocupado"];
 let numeroInicialDeVagas = 5;
@@ -8,10 +10,20 @@ let horarioSelecionado = 0;
 let teste = "";
 let vagas = 0;
 let dataInicial = "";
+let dt_inicio
+let dt_fim
+let disciplina
+let serie
+let professor
+let email
+
 const dropdown_disciplina = document.getElementsByClassName('disc-select')[0];
 const dropdown_serie = document.getElementsByClassName("serie-select")[0];
 const dropdown_data = document.getElementsByClassName("data-select")[0];
+const btn_enviar = document.getElementById("enviar");
 const data = document.getElementById("data");
+const btn_marcar = document.getElementById("marcar-monitoria");
+
 dropdown_serie.addEventListener("change", () => {
     habilitar_filtro_disciplina()
     atualizarDisciplinas(dropdown_disciplina,dropdown_serie.value)
@@ -26,10 +38,20 @@ dropdown_disciplina.addEventListener("change",()=>{
     }
 });
 
+dropdown_data.addEventListener("change",()=>{
+    if(verificar_filtros()){
+        habilitar_btn_enviar()
+    }
+});
+
 dropdown_serie.addEventListener("focus", ()=>{
     atualizarSeries(dropdown_serie)
 
 });
+
+btn_marcar.addEventListener('click',()=>{
+    abrir_popup(dropdown_serie.value,dropdown_disciplina.textContent,professor,email)
+})
 
 btn_enviar.addEventListener('click',()=>{
     capturaHorario(data.value)
