1533c60eae934500f454653368ea164f74cb0f7d
GuLeiteAbreu007 <gustavo.abreu@germinare.org.br>
2024-09-06 15:57:30 -0300

    Consertando bugs do popup

--- js/popup.js
+++ js/popup.js
@@ -5,10 +5,8 @@ const span_serie = document.getElementById('span-serie')
 const span_disc = document.getElementById('span-disc')
 const span_prof = document.getElementById('span-prof')
 const span_email = document.getElementById('span-email')
+const body = document.getElementsByTagName('body')[0]
 
-btn_marcar.addEventListener('click', () => {
-    abrir_popup()
-})
 
 container_popup.addEventListener('click', (event) => {
     if (event.target === container_popup) {
@@ -16,15 +14,26 @@ container_popup.addEventListener('click', (event) => {
     }
 })
 
-function abrir_popup() {
-    container_popup.style.display = 'block'
-    popup.style.display = 'block'
 
-    span_serie.innerText = serie.value+"º"
-    span_disc.innerText = disc.value
+
+function abrir_popup(serie,disciplina,professor,email) {
+    container_popup.style.display = 'flex'
+    popup.style.display = 'block'
+    body.style.position = 'fixed'
+
+    if (serie <=1 && serie >= 3){
+        span_serie.innerText = serie+"ª série"
+    }span_serie.innerText = serie+"º ano"
+
+    span_disc.innerText = disciplina    
+    
+    span_prof.innerText = professor
+
+    span_email.innerText = email
 }
 
 function fechar_popup() {
     container_popup.style.display = 'none'
     popup.style.display = 'none'
+    body.style.position = ''
 }
